d3820800b057da23f52ea68158ff4dda9545f108
NiceGuysDesign <hello@niceguys.agency>
2026-02-23 21:34:46 +0200

    add metadata

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // Потрібно для Heroku: публічний URL додатку (без trailing slash)
+  ...(env('PUBLIC_URL') && { url: env('PUBLIC_URL') }),
   app: {
     keys: env.array('APP_KEYS'),
   },
